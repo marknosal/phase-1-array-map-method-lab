a0faa4c594bbf0e6e411c67f1ee26ff8a156e0c0
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-22 18:36:13 -0600

    So hard...

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// function splitTitle(title){
+//   const splitTitle = title.split(' ');
+//   return splitTitle;
+// }
+
+// function splitWord(wordInTitle){
+//   const splitWord = wordInTitle.split('');
+//   return splitWord;
+// }
+
+// function capitalizeFirstLetter(array) {
+//   const capitalizeFirstLetterOfArray = Object.assign([],array, array[0].toUpperCase());
+//   return capitalizeFirstLetterOfArray;
+// }
+
+// function lettersToWord(array){
+//   const lettersToWord = array.join('');
+//   return lettersToWord;
+// }
+
+// function wordsToTitle(array){
+//   const wordsToTitle = array.join(' ');
+//   return wordsToTitle;
+// }
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(
+     (title) => {
+       const splitTitle = title.split(' ')
+       return splitTitle.map((word) => {
+        const splitWord = word.split('')
+        return Object.assign([], word, splitWord[0].toUpperCase()).join('')
+       }).join(' ')
+     }
+  )
 }
